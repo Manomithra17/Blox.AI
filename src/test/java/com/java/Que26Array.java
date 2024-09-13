ff32639c5e9edb16d0349da41be14fa66a84c42b
Manomithra17 <manogobu@gmail.com>
2024-09-13 22:52:26 +0530

    new changes

---
+++ src/test/java/com/java/Que26Array.java
@@ -0,0 +1,19 @@
+package com.java;
+
+public class Que26Array {
+	
+	public static void main(String[] args) {
+		int a[] = new int[5];
+		a[0] = 10;
+		a[1] = 20;
+		a[2] = 30;
+		a[3] = 40;
+		a[4] = 50;
+
+		for (int i : a) {
+			System.out.println(i);
+
+		}
+
+}
+}
